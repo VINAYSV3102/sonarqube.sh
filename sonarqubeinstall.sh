427cfe6998a8dc253709a42d9ee9881905c34818
VINAYSV3102 <114980298+VINAYSV3102@users.noreply.github.com>
2024-06-19 21:11:29 +0530

    Update sonarqubeinstall.sh

--- sonarqubeinstall.sh
+++ sonarqubeinstall.sh
@@ -13,7 +13,7 @@ sudo yum update -y
      sudo yum install unzip
       sudo unzip /opt/sonarqube-9.3.0.51899.zip
      
-     sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+     sudo chown -R sonar:sonar sonarqube-9.3.0.51899
      cd /opt/sonarqube-x.x/bin/linux-x86-64 
       ./sonar.sh start
     
